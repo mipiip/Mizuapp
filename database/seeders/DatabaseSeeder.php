611a01cdf6b1b813dba0d7de8a5ef1c477f7ad71
mipiip <dreamin93.mh61334@gmail.com>
2023-12-20 17:27:02 +0900

    groupシーダー削除

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            GroupSeeder::class,
             QuestionSeeder::class,
         ]);
         
